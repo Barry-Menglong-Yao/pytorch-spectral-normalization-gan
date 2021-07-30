335ec5c353a1ed33e1e441ce89456b3b034a17a5
Barry Yao <barry.yao.0@gmail.com>
2021-07-30 17:04:48 -0400

    refactor

---
+++ trainer.py
@@ -0,0 +1,78 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import ExponentialLR
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import model_resnet
+import model
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+
+def train(epoch,loader,args,disc_iters,Z_dim,optim_disc,optim_gen,discriminator,generator,scheduler_d,scheduler_g):
+    for batch_idx, (data, target) in enumerate(loader):
+        if data.size()[0] != args.batch_size:
+            continue
+        data, target = Variable(data.cuda()), Variable(target.cuda())
+
+        # update discriminator
+        for _ in range(disc_iters):
+            z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
+            optim_disc.zero_grad()
+            optim_gen.zero_grad()
+            if args.loss == 'hinge':
+                disc_loss = nn.ReLU()(1.0 - discriminator(data)).mean() + nn.ReLU()(1.0 + discriminator(generator(z))).mean()
+            elif args.loss == 'wasserstein':
+                disc_loss = -discriminator(data).mean() + discriminator(generator(z)).mean()
+            else:
+                disc_loss = nn.BCEWithLogitsLoss()(discriminator(data), Variable(torch.ones(args.batch_size, 1).cuda())) + \
+                    nn.BCEWithLogitsLoss()(discriminator(generator(z)), Variable(torch.zeros(args.batch_size, 1).cuda()))
+            disc_loss.backward()
+            optim_disc.step()
+
+        z = Variable(torch.randn(args.batch_size, Z_dim).cuda())
+
+        # update generator
+        optim_disc.zero_grad()
+        optim_gen.zero_grad()
+        if args.loss == 'hinge' or args.loss == 'wasserstein':
+            gen_loss = -discriminator(generator(z)).mean()
+        else:
+            gen_loss = nn.BCEWithLogitsLoss()(discriminator(generator(z)), Variable(torch.ones(args.batch_size, 1).cuda()))
+        gen_loss.backward()
+        optim_gen.step()
+
+        if batch_idx % 100 == 0:
+            print('disc loss', disc_loss.item(), 'gen loss', gen_loss.item())#TODO
+    scheduler_d.step()
+    scheduler_g.step()
+
+def evaluate(epoch,fixed_z,generator):
+
+    samples = generator(fixed_z).cpu().data.numpy()[:64]
+
+
+    fig = plt.figure(figsize=(8, 8))
+    gs = gridspec.GridSpec(8, 8)
+    gs.update(wspace=0.05, hspace=0.05)
+
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.transpose((1,2,0)) * 0.5 + 0.5)
+
+    if not os.path.exists('out/'):
+        os.makedirs('out/')
+
+    plt.savefig('out/{}.png'.format(str(epoch).zfill(3)), bbox_inches='tight')
+    plt.close(fig)
